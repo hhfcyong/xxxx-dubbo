2fda44fae37964240666e108f577da417b403d9e
zhangyong <zhangyongw@yonyou.com>
2017-10-09 13:42:45 +0800

    增加课程文档

---
+++ 课程文档/MyBatis Generator/src/com/bigdata/mybatis/IDao/EnterpriseMapper.java
@@ -0,0 +1,17 @@
+package com.bigdata.mybatis.IDao;
+
+import com.bigdata.mybatis.domain.Enterprise;
+
+public interface EnterpriseMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Enterprise record);
+
+    int insertSelective(Enterprise record);
+
+    Enterprise selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Enterprise record);
+
+    int updateByPrimaryKey(Enterprise record);
+}
