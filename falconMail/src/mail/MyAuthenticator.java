23b1a877d62689724592555b4d70e98c546da17b
zhangyong <zhangyongw@yonyou.com>
2017-11-06 09:50:50 +0800

    add falconmail

---
+++ falconMail/src/mail/MyAuthenticator.java
@@ -0,0 +1,17 @@
+package mail;
+
+import javax.mail.*;  
+public class MyAuthenticator extends Authenticator{   
+    String userName=null;   
+    String password=null;   
+        
+    public MyAuthenticator(){   
+    }   
+    public MyAuthenticator(String username, String password) {    
+        this.userName = username;    
+        this.password = password;    
+    }    
+    protected PasswordAuthentication getPasswordAuthentication(){   
+        return new PasswordAuthentication(userName, password);   
+    }   
+}   
---
+++ falconMail/src/mail/mailServlet.java
@@ -0,0 +1,91 @@
+package mail;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class mailServlet
+ */
+@WebServlet("/mailServlet")
+public class mailServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public mailServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		try {
+			String tos = request.getParameter("tos");
+			String subject = request.getParameter("subject");
+			String content = request.getParameter("content");
+			String[] sourcetos = tos.split(",");
+			// 这个类主要是设置邮件
+			MailSenderInfo mailInfo = new MailSenderInfo();
+			mailInfo.setMailServerHost("邮箱地址");
+			mailInfo.setMailServerPort("25");
+			mailInfo.setValidate(true);
+			mailInfo.setUserName("账号");
+			mailInfo.setPassword("密码");// 您的邮箱密码
+			mailInfo.setFromAddress("发送邮箱");
+			for (int i = 0; i < sourcetos.length; i++) {
+				mailInfo.setToAddress(sourcetos[i]);
+				mailInfo.setSubject(subject);
+				mailInfo.setContent(content);
+				// 这个类主要来发送邮件
+				SimpleMailSender sms = new SimpleMailSender();
+				sms.sendTextMail(mailInfo);// 发送文体格式
+				// SimpleMailSender.sendHtmlMail(mailInfo);//发送html格式
+			}
+		} catch (Exception ex) {
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		try {
+			String tos = request.getParameter("tos");
+			String subject = request.getParameter("subject");
+			String content = request.getParameter("content");
+			String[] sourcetos = tos.split(",");
+			// 这个类主要是设置邮件
+			MailSenderInfo mailInfo = new MailSenderInfo();
+			mailInfo.setMailServerHost("邮箱地址");
+			mailInfo.setMailServerPort("25");
+			mailInfo.setValidate(true);
+			mailInfo.setUserName("邮箱账号");
+			mailInfo.setPassword("密码");// 您的邮箱密码
+			mailInfo.setFromAddress("发送邮箱账号");
+			for (int i = 0; i < sourcetos.length; i++) {
+				mailInfo.setToAddress(sourcetos[i]);
+				mailInfo.setSubject(subject);
+				mailInfo.setContent(content);
+				// 这个类主要来发送邮件
+				SimpleMailSender sms = new SimpleMailSender();
+				sms.sendTextMail(mailInfo);// 发送文体格式
+				// SimpleMailSender.sendHtmlMail(mailInfo);//发送html格式
+			}
+		} catch (Exception ex) {
+		}
+	}
+
+}
